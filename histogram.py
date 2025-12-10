ed24835ed06b6149f56e4fbc8194c69fa61f43d1
Rupesh <rupeshghimire377@gmail.com>
2025-12-10 21:15:11 +0545

    updated histogram

--- histogram.py
+++ histogram.py
@@ -2,6 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 x = np.random.normal(170, 10, 250)
+y = np.random.normal(160, 10, 250)
+z = np.random.normal(180, 10, 250)
+
 
 plt.hist(x)
+plt.hist(y)
+plt.hist(z)
 plt.show()
