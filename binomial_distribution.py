fecf0027bdab1009eee5849d0ac460f0bab8bdb5
Rupesh <rupeshghimire377@gmail.com>
2025-12-11 21:36:33 +0545

    Binomial Distribution

---
+++ binomial_distribution.py
@@ -0,0 +1,20 @@
+# Binomial Distribution
+# It describes the outcome of binary scenarios, e.g. toss of a coin, it will either be head or tails.
+
+# It has three parameters:
+
+# n - number of trials.
+
+# p - probability of occurrence of each trial (e.g. for toss of a coin 0.5 each).
+
+# size - The shape of the returned array.
+
+
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.binomial(n=10, p=0.5, size=1000))
+
+plt.show()
