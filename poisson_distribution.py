bf9d62bbc8bd0c8cd5e155c7214ecd00803806f2
Rupesh <rupeshghimire377@gmail.com>
2025-12-12 22:31:24 +0545

    Difference Between Normal and Binomial Distribution

---
+++ poisson_distribution.py
@@ -0,0 +1,19 @@
+# Poisson Distribution is a Discrete Distribution.
+
+# It estimates how many times an event can happen in a specified time. e.g. If someone eats twice a day what is the probability he will eat thrice?
+
+# It has two parameters:
+
+# lam - rate or known number of occurrences e.g. 2 for above problem.
+
+# size - The shape of the returned array.
+
+
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.poisson(lam=2, size=1000))
+
+plt.show()
