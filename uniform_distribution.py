8d7a3ccc66b7ff13bef3fef5f46ed2acb67bde89
Rupesh <rupeshghimire377@gmail.com>
2025-12-13 21:44:01 +0545

    uniform distribution

---
+++ uniform_distribution.py
@@ -0,0 +1,21 @@
+# Used to describe probability where every event has equal chances of occuring.
+
+# E.g. Generation of random numbers.
+
+# It has three parameters:
+
+# low - lower bound - default 0 .0.
+
+# high - upper bound - default 1.0.
+
+# size - The shape of the returned array.
+
+
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.uniform(size=1000), kind="kde")
+
+plt.show()
