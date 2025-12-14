848f90a3499cce74e26fe97a6167dd1a0e6d9a1d
Rupesh <rupeshghimire377@gmail.com>
2025-12-14 11:02:55 +0545

    uniform distribution

--- difference_between_normalandpoissonDistribution.py
+++ difference_between_normalandpoissonDistribution.py
@@ -4,7 +4,7 @@ import seaborn as sns
 
 data = {
   "normal": random.normal(loc=50, scale=7, size=1000),
-  "poisson": random.poisson(lam=50, size=1000)
+  "poisson": random.poisson(lam=60, size=1000)
 }
 
 sns.displot(data, kind="kde")
