ab078d88d86b70cfbdd0eaf42aaeddd00496cf76
Rupesh <rupeshghimire377@gmail.com>
2025-12-16 22:39:12 +0545

    multinomial distribution

---
+++ multinomial_distribution.py
@@ -0,0 +1,12 @@
+# Multinomial distribution is a generalization of binomial distribution.
+#
+# It describes outcomes of multi-nomial scenarios unlike binomial where scenarios must be only one of two. e.g. Blood type of a population, dice roll outcome.
+#
+# It has three parameters:
+#
+# n - number of times to run the experiment.
+#
+# pvals - list of probabilties of outcomes (e.g. [1/6, 1/6, 1/6, 1/6, 1/6, 1/6] for dice roll).
+#
+# size - The shape of the returned array.
+
