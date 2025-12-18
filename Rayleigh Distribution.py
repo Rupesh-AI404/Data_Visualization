25b6f495353c21e9cfd1856e84b81dc52dde3714
Rupesh <rupeshghimire377@gmail.com>
2025-12-18 23:29:05 +0545

    Rayleigh distribution

---
+++ Rayleigh Distribution.py
@@ -0,0 +1,15 @@
+# Rayleigh distribution is used in signal processing.
+#
+# It has two parameters:
+#
+# scale - (standard deviation) decides how flat the distribution will be default 1.0).
+#
+# size - The shape of the returned array.
+
+from numpy import random
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.displot(random.rayleigh(size=1000), kind="kde")
+
+plt.show()
