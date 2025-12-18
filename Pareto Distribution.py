34004d09e67d018c42abbdfe3eda0369ac62c0f2
Rupesh <rupeshghimire377@gmail.com>
2025-12-18 23:30:08 +0545

    Pareto distribution

--- Pareto Distribution.py
+++ Pareto Distribution.py
@@ -1,3 +1,13 @@
+# A distribution following Pareto's law i.e. 80-20 distribution (20% factors cause 80% outcome).
+#
+# It has two parameter:
+#
+# a - shape parameter.
+#
+# size - The shape of the returned array.
+
+
+
 from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
