8a7c7ad8baa55138bd7dfca4beda06c6dfecd55f
Rupesh <rupeshghimire377@gmail.com>
2025-12-18 23:30:31 +0545

    Pareto distribution

--- Pareto Distribution.py
+++ Pareto Distribution.py
@@ -12,6 +12,6 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.displot(random.pareto(a=2, size=1000))
+sns.displot(random.pareto(a=2, size=2000))
 
 plt.show()
