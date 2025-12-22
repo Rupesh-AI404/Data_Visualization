683a450087a796ed6f513499facd1ddf1c310ec6
Rupesh <rupeshghimire377@gmail.com>
2025-12-22 23:08:51 +0545

    Binomial Distribution

--- binomial_distribution.py
+++ binomial_distribution.py
@@ -15,6 +15,6 @@ from numpy import random
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-sns.displot(random.binomial(n=10, p=0.5, size=1000))
+sns.displot(random.binomial(n=20, p=0.5, size=1000))
 
 plt.show()
