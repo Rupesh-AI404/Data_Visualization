73c705f2fe16778335efcde9df4f6d25f499010e
Rupesh <rupeshghimire377@gmail.com>
2025-12-24 23:23:13 +0545

    Uniform Distribution

--- first.py
+++ first.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 xpoints = np.array([1, 2, 6, 8])
-ypoints = np.array([3, 8, 1, 20])
+ypoints = np.array([3, 8, 1, 40])
 
 plt.plot(xpoints, ypoints)
 plt.show()
