e4270213be5bc0f88f2e0de539a18eb28a4ba9ca
Rupesh <rupeshghimire377@gmail.com>
2025-12-27 23:28:31 +0545

    pie chart

--- pie_chart.py
+++ pie_chart.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-y = np.array([35, 25, 25, 15, 30])
+y = np.array([35, 25, 25, 15, 10])
 mylabels = ["Apples", "Bananas", "cherries", "Dates", "Elderberries"]
 
 plt.pie(y, labels = mylabels)
