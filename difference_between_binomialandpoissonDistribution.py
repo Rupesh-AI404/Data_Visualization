117f47eda5dc0b6a3301d1507286830b0d2a002b
Rupesh <rupeshghimire377@gmail.com>
2026-01-04 18:40:12 +0545

    difference

--- difference_between_binomialandpoissonDistribution.py
+++ difference_between_binomialandpoissonDistribution.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = {
-  "binomial": random.binomial(n=1000, p=0.01, size=2000),
+  "binomial": random.binomial(n=1000, p=0.01, size=3000),
   "poisson": random.poisson(lam=10, size=2000)
 }
 
