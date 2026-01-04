88a4b627ea1e7fbf4bab001a5304fdbbdec5f85b
Rupesh <rupeshghimire377@gmail.com>
2026-01-04 18:40:32 +0545

    difference

--- difference_between_normalandbinomialDistribution.py
+++ difference_between_normalandbinomialDistribution.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 data = {
-  "normal": random.normal(loc=50, scale=5, size=1000),
-  "binomial": random.binomial(n=100, p=0.5, size=1000)
+  "normal": random.normal(loc=50, scale=5, size=2000),
+  "binomial": random.binomial(n=100, p=0.5, size=2000)
 }
 
 sns.displot(data, kind="kde")
